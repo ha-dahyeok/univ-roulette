5491d71253153c484c47bfc5b1045208e053c2b8
Contest User <contest@example.com>
2026-03-11 12:22:52 +0900

    Deploy: Support dynamic PORT for cloud environments

--- app.py
+++ app.py
@@ -139,4 +139,5 @@ def report_price():
             return jsonify({'success': False, 'message': 'Restaurant not found'})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=False, port=port)
